287bdc80c72088188e84197f35d846a580ae62ce
HOGEUN SHIN <57739012+HOGEUNGOD@users.noreply.github.com>
2021-09-06 15:47:30 +0900

    aa

--- main.py
+++ main.py
@@ -1,25 +1,82 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import os
+
+import cv2
+import glob
 import sys
+import os
+import numpy as np
 import tqdm
-import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from tensorflow import keras
 from functools import partial
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from scipy.signal import savgol_filter
 
-(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.fashion_mnist.load_data()
-X_train, X_valid = X_train_full[:-5000], X_train_full[-5000:]
-y_train, y_valid = y_train_full[:-5000], y_train_full[-5000:]
+def load_file(path,x,y,x_1,y_1):
+    files = glob.glob(path+'./*.jpg')
+    if not files:
+        print("Chck Phath")
+        sys.exit()
+    img = []
+    for index, i in enumerate(tqdm.tqdm((files))):
+        im = cv2.imread(i, cv2.IMREAD_GRAYSCALE)[y:y_1, x:x_1]
+        if index == 0:
+            ref_img = im
+        img.append(np.stack([im, ref_img], axis = 2))
+    return np.array(img)
 
-X_mean = X_train.mean(axis=0, keepdims=True)
-X_std = X_train.std(axis=0, keepdims=True) + 1e-7
-X_train = (X_train - X_mean) / X_std
-X_valid = (X_valid - X_mean) / X_std
-X_test = (X_test - X_mean) / X_std
+features = load_file('data/ML/', 211-50, 407-75,211+100,407+75)
+target = np.array(pd.read_csv('data/ML/target.csv', encoding='CP949'))[:,3]
+min_max_scaler  = MinMaxScaler()
+target = min_max_scaler.fit_transform(target.reshape(-1, 1))
+target = savgol_filter(target.flatten(),201,1).reshape(-1, 1)
 
-X_train = X_train[..., np.newaxis]
-X_valid = X_valid[..., np.newaxis]
-X_test = X_test[..., np.newaxis]
+delete_index = [i for i in range(1,439)]
+target = np.delete(target, delete_index, 0)
+features = np.delete(features, delete_index, 0)
+
+X_train,  X_valid, y_train, y_valid = train_test_split(features, target, test_size=0.15, random_state=0)
+X_train = X_train / 255.0
+X_valid = X_valid / 255.0
+
+X_valid,  X_test, y_valid, y_test = train_test_split(X_valid, y_valid, test_size=0.5, random_state=0)
+
+DefaultConv2D = partial(keras.layers.Conv2D,
+                        kernel_size=3, activation='relu', padding="SAME")
+
+model = keras.models.Sequential([
+    DefaultConv2D(filters=64, kernel_size=7, input_shape=[150, 150, 2]),
+    keras.layers.MaxPooling2D(pool_size=2),
+    DefaultConv2D(filters=128),
+    DefaultConv2D(filters=128),
+    keras.layers.MaxPooling2D(pool_size=2),
+    DefaultConv2D(filters=256),
+    DefaultConv2D(filters=256),
+    keras.layers.MaxPooling2D(pool_size=2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(units=128, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(units=64, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(units=1),
+])
 
 
+optimizers = keras.optimizers.Adam()
+
+model.compile(loss="mse", optimizer=optimizers, metrics=["mse"])
+history = model.fit(X_train, y_train, epochs=10, validation_data=(X_valid, y_valid))
+score = model.evaluate(X_valid, y_valid)
+
+training_loss = history.history["loss"]
+test_loss = history.history["val_loss"]
+epoch_count = range(1, len(training_loss)+1)
+
+import matplotlib.pyplot as plt
+plt.plot(epoch_count, training_loss, "r-")
+plt.plot(epoch_count, test_loss, "b-")
+plt.legend(["training_loss", "test_loss"])
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.ylim(0,0.1)
+plt.show()
